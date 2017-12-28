08dd44152e7f669cbae79ecddd61038d788967e0
曹开魁 <caokaikui@51nbapi.com>
2017-12-28 20:59:02 +0800

    调整应用包

--- base/base-model/src/main/java/com/caotao/boot/base/model/Model.java
+++ base/base-model/src/main/java/com/caotao/boot/base/model/Model.java
@@ -2,7 +2,7 @@
  * @company 杭州信牛网络科技有限公司
  * @copyright Copyright (c) 2015-2017
  */
-package com.caotao.boot.common.entity;
+package com.caotao.boot.base.model;
 
 import java.io.Serializable;
 
@@ -10,7 +10,7 @@ import java.io.Serializable;
  * 用途描述
  *
  * @author 曹开魁(Colin)
- * @version $Id: sModel, v0.1 2017年12月26日 15:56 曹开魁(Colin) Exp $
+ * @version $Id: Model, v0.1 2017年12月26日 15:56 曹开魁(Colin) Exp $
  */
-public interface sModel extends Serializable {
+public interface Model extends Serializable {
 }
